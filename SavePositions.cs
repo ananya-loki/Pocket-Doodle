e63e198eb35345dc74e8138efbb8a04e11fc3cff
Rishab-Bhattacharya <122494782+Rishab-Bhattacharya@users.noreply.github.com>
2023-05-27 13:56:58 -0500

    Add files via upload

--- SavePositions.cs
+++ SavePositions.cs
@@ -11,20 +11,39 @@ public class SavePositions : MonoBehaviour
 
     public void Save()
     {
-        List<Dictionary<string, object>> positions = new List<Dictionary<string, object>>(); 
+        List<Dictionary<string, object>> positions = new List<Dictionary<string, object>>();
 
-        foreach (Transform square in squares)
+        // Fetch all active GameObjects in the scene
+        Transform[] allObjects = GameObject.FindObjectsOfType(typeof(Transform)) as Transform[];
+
+        foreach (Transform obj in allObjects)
         {
-            if (square.name.Contains("Square(2)(Clone)")) // Check if the object name contains "Square(2)(Clone)"
+            // Log the name of the GameObject
+            Debug.Log("Found object: " + obj.name);
+
+            // Check if the object name contains "(Clone)"
+            if (obj.name.Contains("(Clone)"))
             {
-                Dictionary<string, object> positionData = new Dictionary<string, object>(); 
-                positionData.Add("name", square.name);
-                positionData.Add("position", new float[] { square.position.x, square.position.y, square.position.z });
+                Debug.Log("dfsf");
+                Dictionary<string, object> positionData = new Dictionary<string, object>();
+                positionData.Add("name", obj.name);
+                positionData.Add("position", new float[] { obj.position.x, obj.position.y, obj.position.z });
                 positions.Add(positionData);
             }
         }
 
-        string json = JsonUtility.ToJson(positions); // Convert list to JSON string
-        File.WriteAllText(Application.dataPath + "/" + fileName, json); // Write JSON string to file
+        string json = JsonUtility.ToJson(positions);
+        Debug.Log(json);
+        File.WriteAllText(Application.dataPath + "/" + fileName, json);
+
+        if (positions.Count > 0)
+        {
+            Debug.Log("Positions saved successfully to " + Application.dataPath + "/" + fileName);
+        }
+        else
+        {
+            Debug.Log("No positions saved. No GameObjects with '(Clone)' in their name found.");
+        }
     }
+
 }
