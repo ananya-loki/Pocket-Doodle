d2b1475d16e3b425711bcb609e8f3351bedf4230
Rishab-Bhattacharya <122494782+Rishab-Bhattacharya@users.noreply.github.com>
2023-07-03 15:00:26 -0500

    Update SavePositions.cs

--- SavePositions.cs
+++ SavePositions.cs
@@ -109,6 +109,74 @@
 //        }
 //
 //    }
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System.IO;
+
+// [System.Serializable]
+// public class PositionData
+// {
+//     public string name;
+//     public float[] position;
+// }
+
+// public class SavePositions : MonoBehaviour
+// {
+//     public Transform[] squares; // Array to hold the square objects
+//     public string fileName = "positions.json"; // Name of the JSON file to save the positions
+
+//     public void Save()
+//     {
+//         List<PositionData> positions = new List<PositionData>();
+
+//         // Fetch all active GameObjects in the scene
+//         Transform[] allObjects = GameObject.FindObjectsOfType(typeof(Transform)) as Transform[];
+
+//         foreach (Transform obj in allObjects)
+//         {
+//             // Log the name of the GameObject
+//             Debug.Log("Found object: " + obj.name);
+
+//             // Check if the object name contains "(Clone)"
+//             if (obj.name.Contains("(Clone)"))
+//             {
+//                 Debug.Log("dfsf");
+//                 PositionData positionData = new PositionData();
+//                 positionData.name = obj.name;
+//                 positionData.position = new float[] { obj.position.x, obj.position.y, obj.position.z };
+//                 positions.Add(positionData);
+//             }
+//         }
+
+//         using (StreamWriter file = new StreamWriter(Application.dataPath + "/" + fileName))
+//         {
+//             file.Write("[\n");
+
+//             for (int i = 0; i < positions.Count; i++)
+//             {
+//                 file.Write("  {\n");
+//                 file.Write($"    \"name\": \"{positions[i].name}\",\n");
+//                 file.Write($"    \"position\": [{positions[i].position[0]}, {positions[i].position[1]}, {positions[i].position[2]}]\n");
+//                 if (i < positions.Count - 1)
+//                     file.Write("  },\n");
+//                 else
+//                     file.Write("  }\n");
+//             }
+
+//             file.Write("]\n");
+//         }
+
+//         if (positions.Count > 0)
+//         {
+//             Debug.Log("Positions saved successfully to " + Application.dataPath + "/" + fileName);
+//         }
+//         else
+//         {
+//             Debug.Log("No positions saved. No GameObjects with '(Clone)' in their name found.");
+//         }
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -119,6 +187,7 @@ public class PositionData
 {
     public string name;
     public float[] position;
+    public float[] color;
 }
 
 public class SavePositions : MonoBehaviour
@@ -145,6 +214,17 @@ public class SavePositions : MonoBehaviour
                 PositionData positionData = new PositionData();
                 positionData.name = obj.name;
                 positionData.position = new float[] { obj.position.x, obj.position.y, obj.position.z };
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer != null) 
+                {
+                    Color color = renderer.material.color; // Get the color of the material
+                    positionData.color = new float[] { color.r, color.g, color.b, color.a }; // RGBA
+                }
+                else
+                {
+                    
+                    positionData.color = new float[] { 1, 1, 1, 1 }; 
+                }
                 positions.Add(positionData);
             }
         }
@@ -158,6 +238,7 @@ public class SavePositions : MonoBehaviour
                 file.Write("  {\n");
                 file.Write($"    \"name\": \"{positions[i].name}\",\n");
                 file.Write($"    \"position\": [{positions[i].position[0]}, {positions[i].position[1]}, {positions[i].position[2]}]\n");
+                file.Write($"    \"color\": [{positions[i].color[0]}, {positions[i].color[1]}, {positions[i].color[2]}, {positions[i].color[3]}]\n");
                 if (i < positions.Count - 1)
                     file.Write("  },\n");
                 else
